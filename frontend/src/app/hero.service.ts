60edfbaaa29a26ab5622037c385ed11cb0c5e350
Ilia Benson <ilia.benson@yahoo.com>
2017-11-25 22:58:45 +0000

    preping backend for api access

--- frontend/src/app/hero.service.ts
+++ frontend/src/app/hero.service.ts
@@ -4,21 +4,37 @@ import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  // getHeroes(): Observable<Hero[]> {
+  //   // Todo: send the message _after_ fetching the heroes
+  //   this.log('fetched heroes');
+  //   return of(HEROES);
+  // }
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     // Todo: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
 <?php
-echo file_get_contents("/home/vagrant/comp586/frontend/dist/index.html");
+// echo file_get_contents("/home/vagrant/comp586/frontend/dist/index.html");
 // readfile("/home/vagrant/comp586/frontend/dist/index.html");
+
+// file_get_contents(base_path() . '/frontend/dist/index.html');
+
+echo "RAWR";
